c5d4270d808f6fb3579119e7282ce355c9f3c920
Stephen Arancio <sjarancio@gmail.com>
2021-10-05 11:46:15 -0400

    Portfolio page

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -1,10 +1,15 @@
 class CryptosController < ApplicationController
   before_action :set_crypto, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
-  
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
   # GET /cryptos or /cryptos.json
   def index
     @cryptos = Crypto.all
+    @url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=89c05170-722e-4b74-85e7-012df26e1894&start=1&limit=5000&convert=USD'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_crypto = JSON.parse(@response)
+    @profit_loss = 0
   end
 
   # GET /cryptos/1 or /cryptos/1.json
@@ -66,5 +71,10 @@ class CryptosController < ApplicationController
     # Only allow a list of trusted parameters through.
     def crypto_params
       params.require(:crypto).permit(:symbol, :user_id, :cost_per, :amount_owned)
+    end
+
+    def correct_user
+      @correct = current_user.cryptos.find_by(id: params[:id])
+      redirect_to cryptos_path, notice: "Not Authorized to edit this entry" if @correct.nil?
     end
 end
